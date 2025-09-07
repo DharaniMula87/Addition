4b233d7588cb76e3b1afc32019ebb437c0dedb9d
DharaniMula87 <dharanimula2006@gmail.com>
2025-09-07 15:11:24 +0530

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -1,1 +1,10 @@
 #include<iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+  cout<<"Enter two numbers a and b:"<<endl;
+  cin>>a>>b;
+  cout<<"Addition of two numbers is "<<a+b<<endl;
+  return 0;
+}
